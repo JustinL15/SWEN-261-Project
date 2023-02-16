0e045da94e37483916f3b29878333ff8726cf108
Matt London <mrl2534@rit.edu>
2023-02-16 17:28:33 -0500

    Write the ProductDAO interface to be extended to write to a file

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/ProductDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/ProductDAO.java
@@ -1,5 +1,65 @@
 package com.estore.api.estoreapi.persistence;
 
-public class ProductDAO {
-    
+import java.io.IOException;
+
+import com.estore.api.estoreapi.model.Product;
+
+/**
+ * Interface for saving a Product to a data store
+ * 
+ * @author Matt London
+ */
+public interface ProductDAO {
+    /**
+     * Retrieves all {@link Product product} from the data store
+     * 
+     * @return Array of {@link Product product} objects, array may be empty
+     * @throws IOException If the data store cannot be accessed
+     */
+    Product[] getProducts() throws IOException;
+
+    /**
+     * Finds all {@link Product product} that match the given text
+     * 
+     * @param text Text that should match all returned
+     * @return Array of matching {@link Product product}s, array may be empty
+     * @throws IOException If the data store cannot be accessed
+     */
+    Product[] findProducts(String text) throws IOException;
+
+    /**
+     * Retrieves a {@link Product product} from the data store
+     * 
+     * @param id ID of the {@link Product product} desired
+     * @return {@link Product product} or null if not found
+     * @throws IOException If the data store cannot be accessed
+     */
+    Product getProduct(int id) throws IOException;
+
+    /**
+     * Saves a {@link Product product} to the data store
+     * 
+     * @param product Created {@link Product product} to save
+     * @return {@link Product product} (null if not found)
+     * @throws IOException If the data store cannot be accessed
+     */
+    Product createProduct(Product product) throws IOException;
+
+    /**
+     * Updates a {@link Product product} in the data store
+     * 
+     * @param product Updated {@link Product product} to save
+     * @return {@link Product product} (null if not able to be updated)
+     * @throws IOException If the data store cannot be accessed
+     */
+    Product updateProduct(Product product) throws IOException;
+
+    /**
+     * Deletes a {@link Product product} from the data store
+     * 
+     * @param id ID of the {@link Product product} to delete
+     * @return True if the ID product was deleted false if not
+     * @throws IOException If the data store cannot be accessed
+     */
+    boolean deleteProduct(int id) throws IOException;
 }
