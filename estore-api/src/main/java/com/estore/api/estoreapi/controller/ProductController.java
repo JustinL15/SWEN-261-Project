d268eb894b02a6491a5bca1a32fa073a2af0aa6a
Alexis Sanders <ars9249@rit.edu>
2023-02-17 11:08:05 -0500

    implemented getProduct(id)

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/ProductController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/ProductController.java
@@ -2,8 +2,12 @@ package com.estore.api.estoreapi.controller;
 
 import com.estore.api.estoreapi.model.Product;
 import com.estore.api.estoreapi.persistence.ProductDAO;
+
+import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Controls the REST requests and responses for a Propduct
  * 
  * @author Matt London
+ * @author Alexis Sanders
  * @author ADD NAMES HERE
  */
 @RestController
@@ -38,10 +43,29 @@ public class ProductController {
         this.productDAO = productDAO;
     }
 
+    /**
+     * Responds to the GET request for a {@linkplain Product product} for the given id
+     * 
+     * @param id The id used to locate the {@link Product product}
+     * 
+     * @return ResponseEntity with {@link Product product} object and the HTTP status of OK if found<br>
+     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProduct(int id) {
-        // TODO implement and write docstring
-        return null;
+        LOG.info("GET /products/" + id);
+        try {
+            Product product = productDAO.getProduct(id);
+            if (product != null)
+                return new ResponseEntity<Product>(product, HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("")
