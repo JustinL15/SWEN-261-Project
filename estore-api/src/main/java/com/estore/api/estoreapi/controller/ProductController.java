9c4e07ec1e3f94482c0243f7af02dd6eb51115e1
Justin Lin <jl4682@rit.edu>
2023-02-17 13:26:25 -0500

    Added the Update and Search Methods

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/ProductController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/ProductController.java
@@ -2,8 +2,12 @@ package com.estore.api.estoreapi.controller;
 
 import com.estore.api.estoreapi.model.Product;
 import com.estore.api.estoreapi.persistence.ProductDAO;
+
+import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -50,10 +54,30 @@ public class ProductController {
         return null;
     }
 
+    /**
+     * Responds to the GET request for all {@linkplain Product products} whose name contains
+     * the text in name
+     * 
+     * @param text The text parameter which contains the text used to find the {@link Product products}
+     * 
+     * @return ResponseEntity with array of {@link Product product} objects (may be empty) and
+     * HTTP status of OK<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     * <p>
+     * Example: Find all products that contain the text "ma"
+     * GET http://localhost:8080/products/?text=ma
+     */
     @GetMapping("/")
     public ResponseEntity<Product[]> searchProducts(@RequestParam String text) {
-        // TODO implement and write docstring
-        return null;
+        LOG.info("GET /products/?text="+text);
+        try {
+            Product[] products = productDAO.findProducts(text);
+            return new ResponseEntity<Product[]>(products,HttpStatus.OK);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("")
@@ -62,10 +86,28 @@ public class ProductController {
         return null;
     }
 
+    /**
+     * Updates the {@linkplain Product product} with the provided {@linkplain Product product} object, if it exists
+     * 
+     * @param product The {@link Product product} to update
+     * 
+     * @return ResponseEntity with updated {@link Product product} object and HTTP status of OK if updated<br>
+     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
     @PutMapping
     public ResponseEntity<Product> updateProduct(@RequestBody Product product) {
-        // TODO implement and write docstring
-        return null;
+        LOG.info("PUT /products " + product);
+        try {
+            if(productDAO.updateProduct(product) != null)
+                return new ResponseEntity<Product>(product,HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @DeleteMapping("/{id}")
