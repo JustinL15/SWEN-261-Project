45c8a4655ba861b8453d009df3137e678d5a05a2
Jessica Eisler <jne1747@g.rit.edu>
2023-02-17 15:04:20 -0500

    Implementing Create and Delete Products

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/ProductController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/ProductController.java
@@ -2,8 +2,12 @@ package com.estore.api.estoreapi.controller;
 
 import com.estore.api.estoreapi.model.Product;
 import com.estore.api.estoreapi.persistence.ProductDAO;
+
+import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,14 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
  * Controls the REST requests and responses for a Propduct
  * 
  * @author Matt London
- * @author ADD NAMES HERE
+ * @author Alexis Sanders
+ * @author Jessica Eisler
  */
 @RestController
 @RequestMapping("product")
 public class ProductController {
     private static final Logger LOG = Logger.getLogger(ProductController.class.getName());
     private ProductDAO productDAO;
-
 
     /// TODO This may need more functions to complete the sprint, these are the template ones I have
     /// written so far
@@ -56,10 +60,32 @@ public class ProductController {
         return null;
     }
 
+    /**
+     * Creates a {@linkplain Product product} with product object
+     * 
+     * @param product - the {@link Product product} to create
+     * @return ResponseEntity with created {@link Product product} object and HTTP
+     *         status of CREATED
+     *         ResponseEntity with HTTP status of CONFLICT if {@link Product
+     *         product} object already exists
+     *         ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
     @GetMapping("")
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-        // TODO implement and write docstring
-        return null;
+        LOG.info("POST /products " + product);
+        try {
+            Product[] products = productDAO.getProducts();
+            for (Product product2 : products) {
+                if (product.getName().equals(product2.getName()) || product.getId() == product2.getId()) {
+                    return new ResponseEntity<>(HttpStatus.CONFLICT);
+                }
+            }
+            productDAO.createProduct(product);
+            return new ResponseEntity<Product>(product, HttpStatus.CREATED);
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PutMapping
@@ -68,9 +94,28 @@ public class ProductController {
         return null;
     }
 
+    /**
+     * Deletes a {@linkplain Product product} with the given id
+     * 
+     * @param id - the id of the {@link Product product} to delete
+     * @return ResponseEntity HTTP status of OK if deleted
+     *         ResponseEntity with HTTP status of NOT_FOUND if not found
+     *         ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Product> deleteProduct(int id) {
-        // TODO implement and write docstring
-        return null;
+        LOG.info("Delete /products/" + id);
+        try {
+            Product product = productDAO.getProduct(id);
+            if (product != null) {
+                productDAO.deleteProduct(id);
+                return new ResponseEntity<>(HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
