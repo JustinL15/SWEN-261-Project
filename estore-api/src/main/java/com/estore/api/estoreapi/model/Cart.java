e5a1c7dcbe192ed4d31bf802c79b0912222f9827
Matt London <mrl2534@rit.edu>
2023-03-01 15:07:56 -0500

    Allow cart to more easily interact with frontend

--- estore-api/src/main/java/com/estore/api/estoreapi/model/Cart.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Cart.java
@@ -1,8 +1,6 @@
 package com.estore.api.estoreapi.model;
 
-
-import java.util.List;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.logging.Logger;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -15,15 +13,16 @@ public class Cart {
     private static final Logger LOG = Logger.getLogger(Product.class.getName());
 
     // All cart parameters
-    @JsonProperty("inventory") private List<ProductReference> inventory;
-    @JsonProperty("total") private double total;
+    @JsonProperty("id") private int id;
+    // Maps product id to quantity
+    @JsonProperty("inventory") private HashMap<Integer, ProductReference> inventory;
 
     /**
      * Constructor for the cart, creates an empty cart
      */
-    public Cart() {
-        this.inventory = new ArrayList<ProductReference>();
-        this.total = 0;
+    public Cart(@JsonProperty("id") int id) {
+        this.id = id;
+        this.inventory = new HashMap<>();
     }
 
     /**
@@ -31,29 +30,52 @@ public class Cart {
      * 
      * @return Inventory of the cart
      */
-    public List<ProductReference> getInventory() {
+    public HashMap<Integer, ProductReference> getInventory() {
         return inventory;
-    }
-
-    /**
-     * Adds a product to the cart
-     * 
-     * @param item Product reference to add
-     */
-    public void addItem(ProductReference item) {
-        inventory.add(item);
-        // TODO reference the productDAO so that we can get the price
-        // TODO reference the productDAO so we can subtract from quantity
     }
 
     /**
      * Adds a product to the cart by calling the ProductReference add method
      * 
+     * Checks to see if it already exists and if we should just add on to the quantity
+     * 
      * @param productId Id of the product to add
      * @param quantity Quantity of the product to add
      */
     public void addItem(int productId, int quantity) {
-        inventory.add(new ProductReference(productId, quantity));
+        if (inventory.containsKey(productId)) {
+            int currentQuantity = inventory.get(productId).getQuantity();
+            inventory.get(productId).setQuantity(currentQuantity + quantity);
+        }
+        else {
+            inventory.put(productId, new ProductReference(productId, quantity));
+        }
+    }
+
+    /**
+     * Edit the quantity of a product reference in the inventory
+     * 
+     * @param productId Id of the product to edit
+     * @param quantity Quantity to set the product to, 0 will remove it
+     */
+    public void editQuantity(int productId, int quantity) {
+        if (inventory.containsKey(productId)) {
+            if (quantity == 0) {
+                inventory.remove(productId);
+            }
+            else {
+                inventory.get(productId).setQuantity(quantity);
+            }
+        }
+    }
+
+    /**
+     * Remove an item from the cart
+     * 
+     * @param productId Id of the product to remove
+     */
+    public void removeItem(int productId) {
+        editQuantity(productId, 0);
     }
 
     /**
@@ -61,6 +83,14 @@ public class Cart {
      */
     public void clear() {
         inventory.clear();
-        total = 0;
+    }
+
+    /**
+     * Get the id of the cart
+     * 
+     * @return Id of the cart
+     */
+    public int getId() {
+        return id;
     }
 }
