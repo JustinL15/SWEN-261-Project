391ddc29dd8045836143671deb2d9c8b2c05ece4
Justin Lin <jl4682@rit.edu>
2023-03-03 00:36:18 -0500

    Implemented a product list view

--- estore-ui/src/app/app-routing.module.ts
+++ estore-ui/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
-  { path: 'home', component: DashboardComponent}
+  { path: 'home', component: DashboardComponent},
+  { path: 'products', component: ProductsComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
