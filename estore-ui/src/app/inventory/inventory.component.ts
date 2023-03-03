9f3e889a35c1bc138fc2ac5e1cd7e55a27d3a34e
Jessica Eisler <jne1747@g.rit.edu>
2023-03-03 16:20:56 -0500

    display inventory as a list - will be attached to an owner only tab

---
+++ estore-ui/src/app/inventory/inventory.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Product } from '../product';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-inventory',
+  templateUrl: './inventory.component.html',
+  styleUrls: ['./inventory.component.css']
+})
+export class InventoryComponent implements OnInit{
+
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productService.getProducts().subscribe(products => this.products = products);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.productService.addProduct({ name } as Product).subscribe(product => { this.products.push(product) });
+  }
+
+  delete(product: Product): void {
+    this.products = this.products.filter(p => p !== product);
+    this.productService.deleteProduct(product.id).subscribe();
+  }
+
+}
