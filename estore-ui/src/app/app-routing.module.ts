5fd453f5fd952b13c60dd8d7453c133bb3942a65
Alexis Sanders <ars9249@rit.edu>
2023-03-04 10:36:30 -0500

    Created link to empty cart page

--- estore-ui/src/app/app-routing.module.ts
+++ estore-ui/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
   { path: 'home', component: DashboardComponent},
   { path: 'products', component: ProductsComponent},
-  { path: '', redirectTo: '/home', pathMatch: 'full'}
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'cart', component: ShoppingCartComponent }
 ];
 
 @NgModule({
