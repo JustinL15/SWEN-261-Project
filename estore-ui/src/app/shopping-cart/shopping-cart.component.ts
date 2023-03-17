71a1cf817577b8629f2ccef4734bc781fa1ad632
Alexis Sanders <ars9249@rit.edu>
2023-03-17 12:29:43 -0400

    changed cart to not be automatically set

--- estore-ui/src/app/shopping-cart/shopping-cart.component.ts
+++ estore-ui/src/app/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Cart } from '../cart';
 import { CartService } from '../cart.service';
 import { Order } from '../order';
@@ -12,9 +13,10 @@ import { ProductService } from '../product.service';
   styleUrls: ['./shopping-cart.component.css']
 })
 export class ShoppingCartComponent {
-  cart: Cart = {id: -1, inventory: {}}; // | undefined, but unsure how to change rest
+  cart: Cart | undefined;
 
   constructor(
+    private route: ActivatedRoute,
     private cartService:CartService, 
     private productService: ProductService
     ) { }
@@ -26,7 +28,8 @@ export class ShoppingCartComponent {
 
 
   getCart(): void {
-    this.cartService.getCart(this.cart.id).subscribe((cart: Cart) => this.cart = cart);
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10); 
+    this.cartService.getCart(id).subscribe((cart: Cart) => this.cart = cart);
   }
 
   increase(product: ProductReference): void {
@@ -51,54 +54,59 @@ export class ShoppingCartComponent {
   }
 
   remove(product: ProductReference): void {
-    delete this.cart.inventory[product.id];
+    if(this.cart){
+      delete this.cart.inventory[product.id];
+    }
   } 
 
   checkout(): void {
     // loop through the current number of products in the cart
     var totalPrice = 0;
     var products = [];
+    if(this.cart){
+      for (const prodId in this.cart.inventory){
+        // get the current number of products in the cart
+        var inStock = 0;
+        var orderQuantity = 0;
+        var productPrice = 0;
+        var productId: number =+prodId;
+        this.productService.getProduct(productId).subscribe(prod => inStock = prod.quantity);
+        this.productService.getProduct(productId).subscribe(prod => productPrice = prod.price);
 
-    for (const prodId in this.cart.inventory){
-      // get the current number of products in the cart
-      var inStock = 0;
-      var orderQuantity = 0;
-      var productPrice = 0;
-      var productId: number =+prodId;
-      this.productService.getProduct(productId).subscribe(prod => inStock = prod.quantity);
-      this.productService.getProduct(productId).subscribe(prod => productPrice = prod.price);
+        // check if any items are in inventory
+        if(inStock < 1){
+          break;
+        }
 
-      // check if any items are in inventory
-      if(inStock < 1){
-        break;
+        // check if limited items are in inventory
+        var num: number;
+        var inventory = this.cart.inventory;
+        Object.keys(this.cart.inventory).find(key => orderQuantity = inventory[(num=+key)].quantity);
+        if (inStock < orderQuantity){
+          orderQuantity = inStock;
+          Object.keys(this.cart.inventory).find(key => inventory[(num=+key)].quantity = 0);
+          this.cart.inventory = inventory;
+        } else{
+          Object.keys(this.cart.inventory).find(key => inventory[(num=+key)].quantity -= orderQuantity);
+          this.cart.inventory = inventory;
+        }
+        totalPrice += orderQuantity * productPrice;
+
+        // create snapshop of each product and quantity in cart
+        var product: Product = {id: -1, name: "", price: -1, quantity: -1, description: ""};
+        this.productService.getProduct(productId).subscribe(prod => product = prod);
+        products.push(product);
       }
 
-      // check if limited items are in inventory
-      var num: number;
-      Object.keys(this.cart.inventory).find(key => orderQuantity = this.cart.inventory[(num=+key)].quantity);
-      if (inStock < orderQuantity){
-        orderQuantity = inStock;
-        Object.keys(this.cart.inventory).find(key => this.cart.inventory[(num=+key)].quantity = 0);
-      } else{
-        Object.keys(this.cart.inventory).find(key => this.cart.inventory[(num=+key)].quantity -= orderQuantity);
+      // clear shopping cart
+      this.cart.inventory = {};
+
+      // create an order
+      if(products.length >= 1){
+        this.cartService.createOrder(
+          { id: this.cart.id, totalPrice: totalPrice, products: products, dateTime: null } as Order
+          );
       }
-      totalPrice += orderQuantity * productPrice;
-
-      // create snapshop of each product and quantity in cart
-      var product: Product = {id: -1, name: "", price: -1, quantity: -1, description: ""};
-      this.productService.getProduct(productId).subscribe(prod => product = prod);
-      products.push(product);
-    }
-
-    // clear shopping cart
-    this.cart.inventory = {};
-
-    // create an order
-    if(products.length >= 1){
-      this.cartService.createOrder(
-        { id: this.cart.id, totalPrice: totalPrice, products: products, dateTime: null } as Order
-        );
     }
   }
-
 }
