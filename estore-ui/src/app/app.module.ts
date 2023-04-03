a3ae9837f96cc1a2edec996a3c21a26b9ecd8586
Matt London <mrl2534@rit.edu>
2023-04-03 16:09:14 -0400

    Fix spacing to conform to standards

--- estore-ui/src/app/app.module.ts
+++ estore-ui/src/app/app.module.ts
@@ -20,9 +20,9 @@ import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 
