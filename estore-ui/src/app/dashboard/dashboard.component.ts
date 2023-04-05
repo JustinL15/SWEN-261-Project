74da42bd34eb78487a0a18e0e9e7ad08d8be30e1
Alexis Sanders <ars9249@rit.edu>
2023-04-05 01:10:41 -0400

    changed method to createRecommended

--- estore-ui/src/app/dashboard/dashboard.component.ts
+++ estore-ui/src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { Product } from '../product';
 import { CategoryService } from '../services/category.service'
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -20,17 +21,34 @@ export class DashboardComponent{
   }
 
   async createRecommended(): Promise<void> {
-    var user = await this.userService.getCurrentUser();
+    let user = this.userService.getCurrentUser();
     if(user !== null && user !== undefined){
-      const min = 0;
-      const starredMax = user.starred.length - 1;
-      const starredInt = Math.floor(Math.random() * (starredMax - min + 1)) + min;
-      var category = user.starred[starredInt].category;
-      var productsInCategory: Product[] = [];
-      this.categoryService.getProductsByCategory(category).subscribe(prods => productsInCategory = prods);
-      const prodMax = this.products.length - 1; 
-      const prodInt = Math.floor(Math.random() * (prodMax - min + 1)) + min;
-      this.products.push(productsInCategory[prodInt])
+      if(user.starred !== undefined){
+        const min = 0;
+        const starredMax = user.starred.length - 1;
+        const starredInt = Math.floor(Math.random() * (starredMax - min + 1)) + min;
+        var category = user.starred[starredInt].category;
+        var productsInCategory: Product[] = [];
+        this.categoryService.getProductsByCategory(category).subscribe(products => {
+        productsInCategory = products;
+        const prodMax = productsInCategory.length - 1; 
+        const prodInt = Math.floor(Math.random() * (prodMax - min + 1)) + min;
+        this.products.push(productsInCategory[prodInt]);
+        console.log(prodInt);
+        console.log(productsInCategory);
+        console.log(productsInCategory[prodInt]);
+        console.log(this.products);
+       })
+        // const prodMax = productsInCategory.length - 1; 
+        // const prodInt = Math.floor(Math.random() * (prodMax - min + 1)) + min;
+        // this.products.push(productsInCategory[prodInt])
+
+        // this.categoryService.getProductsByCategory(category).subscribe
+        // console.log(prodInt);
+        // console.log(productsInCategory[prodInt]);
+        // console.log(productsInCategory);
+        // console.log(this.products);
+      }
     }
   }
 }
