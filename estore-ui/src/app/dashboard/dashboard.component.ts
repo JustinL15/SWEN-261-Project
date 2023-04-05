e083681ba0de0fb7f1caf2955840f331fe26cd1f
Matt London <mrl2534@rit.edu>
2023-04-05 01:54:17 -0400

    Fix recommendation system

--- estore-ui/src/app/dashboard/dashboard.component.ts
+++ estore-ui/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { Product } from '../product';
+import { ProductService } from '../services/product.service';
 import { CategoryService } from '../services/category.service'
 import { firstValueFrom } from 'rxjs';
 
@@ -10,10 +11,11 @@ import { firstValueFrom } from 'rxjs';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent{
-  products: Product[] = [];
+  productCopies: Product[] = [];
   constructor(
     public userService: UserService,
-    public categoryService: CategoryService
+    public categoryService: CategoryService,
+    public productService: ProductService
     ) {}
 
   ngOnInit(): void {
@@ -21,34 +23,73 @@ export class DashboardComponent{
   }
 
   async createRecommended(): Promise<void> {
-    let user = this.userService.getCurrentUser();
-    if(user !== null && user !== undefined){
-      if(user.starred !== undefined){
-        const min = 0;
-        const starredMax = user.starred.length - 1;
-        const starredInt = Math.floor(Math.random() * (starredMax - min + 1)) + min;
-        var category = user.starred[starredInt].category;
-        var productsInCategory: Product[] = [];
-        this.categoryService.getProductsByCategory(category).subscribe(products => {
-        productsInCategory = products;
-        const prodMax = productsInCategory.length - 1; 
-        const prodInt = Math.floor(Math.random() * (prodMax - min + 1)) + min;
-        this.products.push(productsInCategory[prodInt]);
-        console.log(prodInt);
-        console.log(productsInCategory);
-        console.log(productsInCategory[prodInt]);
-        console.log(this.products);
-       })
-        // const prodMax = productsInCategory.length - 1; 
-        // const prodInt = Math.floor(Math.random() * (prodMax - min + 1)) + min;
-        // this.products.push(productsInCategory[prodInt])
+    // Get current user
+    let customer = this.userService.getCurrentUser();
 
-        // this.categoryService.getProductsByCategory(category).subscribe
-        // console.log(prodInt);
-        // console.log(productsInCategory[prodInt]);
-        // console.log(productsInCategory);
-        // console.log(this.products);
+    // Just display welcome page if it is null
+    if (customer == null) {
+      return;
+    }
+
+    // Get a list of the categories of all starred products
+    let categories: string[] = [];
+    for (let product of customer.starred) {
+      // Get the category of the product
+      let category = (await firstValueFrom(this.productService.getProduct(product.id))).category;
+
+      // Now add this category to our set
+      // Only add if it isn't already in the set
+      if (!categories.includes(category)) {
+        categories.push(category);
       }
+
+    }
+
+    // If we have no categories then exit
+    if (categories.length == 0) {
+      return;
+    }
+
+    // Okay now we have a list of these categories. Randomly select three
+    let selectedCategories: string[] = [];
+    for (let category of categories) {
+      // Randomly select a category
+      let random = Math.floor(Math.random() * categories.length);
+      if (random < 3 && !selectedCategories.includes(category)) {
+        selectedCategories.push(category);
+      }
+    }
+
+    // If none were added then we will add the first one
+    if (selectedCategories.length == 0) {
+      selectedCategories.push(categories[0]);
+    }
+
+    // Now we have a list of categories, we want random products in those categories
+    let selectedProducts: number[] = [];
+    for (let category of selectedCategories) {
+      // Get a list of products in this category
+      let products = await firstValueFrom(this.categoryService.getProductsByCategory(category.toString()));
+
+      // Now take two products randomly
+      for (let product of products) {
+        let random = Math.floor(Math.random() * products.length);
+        if (random < 2 && !selectedProducts.includes(product.id)) {
+          selectedProducts.push(product.id);
+        }
+      }
+
+      // If none were added then we will add the first one
+      if (selectedProducts.length == 0 && products.length > 0) {
+        selectedProducts.push(products[0].id);
+      }
+    }
+    
+    // Now we should have a list of products to display
+    // Load them into product copies
+    for (let productId of selectedProducts) {
+      let product = await firstValueFrom(this.productService.getProduct(productId));
+      this.productCopies.push(product);
     }
   }
 }
