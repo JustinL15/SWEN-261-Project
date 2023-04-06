7422a4b351de1321fc9ea3df244e1af1159d0b73
Justin Lin <jl4682@rit.edu>
2023-04-05 21:41:49 -0400

    fixed login

--- estore-ui/src/app/login.guard.ts
+++ estore-ui/src/app/login.guard.ts
@@ -6,25 +6,30 @@ import { LoginDialogComponent } from './login-dialog/login-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Injectable({
-    providedIn: 'root'
-  })
-  export class LoginGuard implements CanActivate {
-  
-    constructor(
-      private userService: UserService,
-      private matDialog: MatDialog
-      ) {}
-  
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      const isLoggedIn = this.userService.isLoggedIn();
-      if (isLoggedIn) {
-        return true;
-      } else {
-        // opens up a login dialog
-        this.matDialog.open(LoginDialogComponent);
-        return false;
-      }
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+
+  constructor(
+    private userService: UserService,
+    private matDialog: MatDialog,
+    private router: Router
+  ) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const isLoggedIn = this.userService.isLoggedIn();
+    if (isLoggedIn) {
+      return true;
+    } else {
+      // opens up a login dialog
+      let login = this.matDialog.open(LoginDialogComponent);
+      login.afterClosed().subscribe(() => {
+        // User logged in successfully, navigate to the current route again to reload the page with the new user data
+        window.history.back();
+      });
+      return false;
     }
-  
   }
+
+}
   
